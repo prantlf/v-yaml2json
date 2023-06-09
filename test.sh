b6133402e2351bfbb32fb9de5f1a3a10c77e0d23
Ferdinand Prantl <prantlf@gmail.com>
2023-06-09 09:52:27 +0200

    test: Improve the test output

--- test.sh
+++ test.sh
@@ -2,8 +2,25 @@
 
 set -e
 
+test() {
+  if [ "$2" != "" ]; then
+    echo "----------------------------------------"
+  fi
+  echo "$1"
+  echo "----------------------------------------"
+}
+
+test "help"
 ./yaml2json -h
+
+test "version" 1
 ./yaml2json -V
+
+test "stdin and stdout" 1
 cat .github/workflows/ci.yml | ./yaml2json > /dev/null
+
+test "file input and output"
 ./yaml2json .github/workflows/ci.yml -l -p -o ci.json
 rm ci.json
+
+echo "done"
